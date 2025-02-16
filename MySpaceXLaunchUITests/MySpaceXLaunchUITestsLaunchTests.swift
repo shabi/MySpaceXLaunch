f28da9fbaab95f49125867055cfae11277f4d66a
shabi <naqvi.shabi@gmail.com>
2025-02-16 14:19:56 +0530

    Update MySpaceXLaunchUITestsLaunchTests.swift

--- MySpaceXLaunchUITests/MySpaceXLaunchUITestsLaunchTests.swift
+++ MySpaceXLaunchUITests/MySpaceXLaunchUITestsLaunchTests.swift
@@ -41,7 +41,7 @@ public class CardAccountViewModel: ObservableObject {
     }
 
     private func loadTestDataIfAvailable() {
-        if let jsonString = ProcessInfo.processInfo.environment["TEST_ACCOUNT_DATA"],
+        if let jsonString = UserDefaults.standard.string(forKey: "accountData"),
            let data = jsonString.data(using: .utf8),
            let decodedData = try? JSONDecoder().decode(CardInfoModel.self, from: data) {
             DispatchQueue.main.async {
@@ -58,14 +58,72 @@ import { driver } from "@wdio/globals";
 export async function setAccountData(testData: object) {
     const jsonString = JSON.stringify(testData);
 
-    // Terminate and relaunch app with test data
-    await driver.terminateApp("com.yourapp.bundleid");
-    await driver.activateApp("com.yourapp.bundleid", {
-        arguments: ["-TEST_ACCOUNT_DATA", jsonString]
-    });
+    await driver.executeScript(`
+        const userDefaults = NSUserDefaults.standardUserDefaults();
+        userDefaults.setObjectForKey('${jsonString}', 'accountData');
+        userDefaults.synchronize();
+    `);
 
     console.log("Test Data Set:", jsonString);
 }
+
+//
+
+await driver.executeScript(`
+    objc.import("Foundation");
+    let userDefaults = $.NSUserDefaults.standardUserDefaults;
+    userDefaults.setObjectForKey("${jsonString}", "accountData");
+    userDefaults.synchronize();
+`);
+
+import { Given, When, Then } from "@wdio/cucumber-framework";
+import { setAccountData } from "../pageObjects/IosAccountPage";
+import { expect } from "chai";
+
+Given(/^user "([^"]*)" is logged into the Mobile App$/, async function (userId) {
+    let testData;
+
+    switch (userId) {
+        case "TRUNC67":
+            testData = {
+                accountNameText: "Transaction Banking Business On(...)",
+                accountAmountText: "72,000.60 AED",
+                accountNumberText: "1011000915221",
+                accountTypeText: "CURRENT ACCOUNT"
+            };
+            break;
+        case "MillionBalance":
+            testData = {
+                accountNameText: "Transaction Banking Business",
+                accountAmountText: "1.12M AED",
+                accountNumberText: "4011000915200002",
+                accountTypeText: "LC SIGHT"
+            };
+            break;
+        default:
+            throw new Error(`No test data found for user: ${userId}`);
+    }
+
+    await setAccountData(testData);
+});
+
+When(/^the user navigates to the Accounts tab$/, async function () {
+    await $("~AccountsTab").click();
+});
+
+Then(/^the user should see the following values in the blue card:$/, async function (dataTable) {
+    const expectedData = dataTable.rowsHash();
+
+    const actualAccountName = await $("~accountNameText").getText();
+    const actualBalance = await $("~accountAmountText").getText();
+    const actualAccountNumber = await $("~accountNumberText").getText();
+    const actualAccountType = await $("~accountTypeText").getText();
+
+    expect(actualAccountName).to.equal(expectedData.accountName);
+    expect(actualBalance).to.equal(expectedData.accountBalance);
+    expect(actualAccountNumber).to.equal(expectedData.accountNumber);
+    expect(actualAccountType).to.equal(expectedData.accountType);
+});
 
 
 
